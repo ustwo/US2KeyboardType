f83638d26257136a190e2aaa23bda5991ab2a13b
Martin Stolz <martin@ustwo.co.uk>
2014-05-29 14:17:24 +0100

    Added initial project

---
+++ US2KeyboardType/US2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  US2ViewController.h
+//  US2KeyboardType
+//
+//  Created by Martin on 29/05/2014.
+//  Copyright (c) 2014 ustwo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface US2ViewController : UIViewController
+
+@end
---
+++ US2KeyboardType/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  US2KeyboardType
+//
+//  Created by Martin on 29/05/2014.
+//  Copyright (c) 2014 ustwo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "US2AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([US2AppDelegate class]));
+    }
+}
